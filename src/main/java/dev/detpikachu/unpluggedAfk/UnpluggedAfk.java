425569e1056471f62ffb23b91719c0242d65906e
Andrei Hava <andrei@omnilium.com>
2026-08-13 20:24:59 +0300

    initial commit

---
+++ src/main/java/dev/detpikachu/unpluggedAfk/UnpluggedAfk.java
@@ -0,0 +1,26 @@
+package dev.detpikachu.unpluggedAfk;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UnpluggedAfk extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+    }
+}
