5c553efaeb24761812a98b884ee74f03990932f6
Andrei Hava <andrei@omnilium.com>
2026-08-14 14:13:46 +0300

    wip

--- src/main/java/dev/detpikachu/unpluggedAfk/commands/admin/debug/AdminDebugDummyCommand.java
+++ src/main/java/dev/detpikachu/unpluggedAfk/commands/admin/debug/AdminDebugDummyCommand.java
@@ -1,24 +1,14 @@
 package dev.detpikachu.unpluggedAfk.commands.admin.debug;
 
-import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
-import dev.detpikachu.unpluggedAfk.network.UnpluggedConnection;
-import dev.detpikachu.unpluggedAfk.network.UnpluggedGamePacketListener;
-import dev.detpikachu.unpluggedAfk.player.UnpluggedServerPlayer;
+import dev.detpikachu.unpluggedAfk.UnpluggedPlayerManager;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
-import io.papermc.paper.util.KeepAlive;
-import java.util.HashSet;
 import java.util.UUID;
-import net.minecraft.network.protocol.PacketFlow;
-import net.minecraft.server.level.ClientInformation;
-import net.minecraft.server.network.CommonListenerCookie;
-import net.minecraft.world.level.GameType;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.craftbukkit.CraftWorld;
-import org.bukkit.event.player.PlayerGameModeChangeEvent;
 
 public final class AdminDebugDummyCommand {
 
@@ -38,18 +28,9 @@ public final class AdminDebugDummyCommand {
         var server = ((CraftServer) Bukkit.getServer()).getServer();
         var executor = context.getSource().getExecutor();
         var level = ((CraftWorld) executor.getWorld()).getHandle();
-        var playerList = server.getPlayerList();
 
-        var profile = new GameProfile(UUID.fromString(DUMMY_UUID), DUMMY_NAME);
-        var clientInformation = ClientInformation.createDefault();
-        var cookie = new CommonListenerCookie(profile, 0, clientInformation, true, null, new HashSet<>(), new KeepAlive());
-        var connection = new UnpluggedConnection(PacketFlow.SERVERBOUND);
-        var bot = new UnpluggedServerPlayer(server, level, profile, clientInformation);
-
-        playerList.placeNewPlayer(connection, bot, cookie);
-        bot.connection = new UnpluggedGamePacketListener(server, connection, bot, cookie);
-        bot.snapTo(executor.getX(), executor.getY(), executor.getZ(), executor.getYaw(), executor.getPitch());
-        bot.gameMode.changeGameModeForPlayer(GameType.DEFAULT_MODE, PlayerGameModeChangeEvent.Cause.DEFAULT_GAMEMODE, null);
+        final var unpluggedPlayer = UnpluggedPlayerManager.createDummy(server, level, UUID.fromString(DUMMY_UUID), DUMMY_NAME);
+        unpluggedPlayer.snapTo(executor.getX(), executor.getY(), executor.getZ(), executor.getYaw(), executor.getPitch());
 
         return 1;
     }
