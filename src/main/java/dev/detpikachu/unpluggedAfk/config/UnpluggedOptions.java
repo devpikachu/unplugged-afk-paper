970aa62c4c413b7c4ffd1a2d6f4b3f8aa5f03d26
Andrei Hava <andrei@omnilium.com>
2026-08-15 13:26:01 +0300

    make config getters pure

--- src/main/java/dev/detpikachu/unpluggedAfk/config/UnpluggedOptions.java
+++ src/main/java/dev/detpikachu/unpluggedAfk/config/UnpluggedOptions.java
@@ -1,8 +1,9 @@
 package dev.detpikachu.unpluggedAfk.config;
 
+import dev.detpikachu.unpluggedAfk.UnpluggedAfk;
 import org.bukkit.configuration.file.FileConfiguration;
 
-import static dev.detpikachu.unpluggedAfk.UnpluggedConstants.DEFAULT_DURATION_MINS;
+import static dev.detpikachu.unpluggedAfk.UnpluggedConstants.DEFAULT_MAX_DURATION_MINS;
 
 public final class UnpluggedOptions {
 
@@ -11,37 +12,33 @@ public final class UnpluggedOptions {
     private static final String KEY_DEFAULT_DURATION_MINS = "defaultDurationMins";
     private static final String KEY_MAX_DURATION_MINS = "maxDurationMins";
 
-    private int defaultDurationMins = DEFAULT_DURATION_MINS;
-    private int maxDurationMins = DEFAULT_DURATION_MINS;
+    private int defaultDurationMins = DEFAULT_MAX_DURATION_MINS;
+    private int maxDurationMins = DEFAULT_MAX_DURATION_MINS;
 
     public static UnpluggedOptions getInstance() {
         return INSTANCE;
     }
 
     public int getDefaultDurationMins() {
-        // TODO: This is less than ideal since this method has side-effects. Good enough for MVP.
-        if (this.defaultDurationMins <= 0) {
-            this.defaultDurationMins = DEFAULT_DURATION_MINS;
-        }
-
-        if (this.defaultDurationMins > this.getMaxDurationMins()) {
-            this.defaultDurationMins = this.maxDurationMins;
-        }
-
         return this.defaultDurationMins;
     }
 
     public int getMaxDurationMins() {
-        // TODO: This is less than ideal since this method has side-effects. Good enough for MVP.
-        if (this.maxDurationMins <= 0) {
-            this.maxDurationMins = DEFAULT_DURATION_MINS;
-        }
-
         return this.maxDurationMins;
     }
 
     public static void deserialize(FileConfiguration config) {
-        INSTANCE.defaultDurationMins = config.getInt(KEY_DEFAULT_DURATION_MINS, DEFAULT_DURATION_MINS);
-        INSTANCE.maxDurationMins = config.getInt(KEY_MAX_DURATION_MINS, DEFAULT_DURATION_MINS);
+        INSTANCE.defaultDurationMins = config.getInt(KEY_DEFAULT_DURATION_MINS, DEFAULT_MAX_DURATION_MINS);
+        INSTANCE.maxDurationMins = config.getInt(KEY_MAX_DURATION_MINS, DEFAULT_MAX_DURATION_MINS);
+
+        if (INSTANCE.maxDurationMins < 1) {
+            UnpluggedAfk.LOGGER.warn("Max duration of {} is invalid. The value must be greater than or equal to 1. Resetting to {}.", INSTANCE.maxDurationMins, DEFAULT_MAX_DURATION_MINS);
+            INSTANCE.maxDurationMins = DEFAULT_MAX_DURATION_MINS;
+        }
+
+        if (INSTANCE.defaultDurationMins < 1 || INSTANCE.defaultDurationMins > INSTANCE.maxDurationMins) {
+            UnpluggedAfk.LOGGER.warn("Default duration of {} is invalid. The value must be between 1 and {}. Resetting to {}.", INSTANCE.defaultDurationMins, INSTANCE.maxDurationMins, INSTANCE.maxDurationMins);
+            INSTANCE.defaultDurationMins = INSTANCE.maxDurationMins;
+        }
     }
 }
